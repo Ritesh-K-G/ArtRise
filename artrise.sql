8a6d2160989bd63e2d391c3c4c871a3feec96aae
parthgarg497 <parthgarg497@gmail.com>
2023-05-03 23:53:40 +0530

    added some library files for email verification

--- artrise.sql
+++ artrise.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 02, 2023 at 01:11 PM
+-- Generation Time: Apr 29, 2023 at 12:11 PM
 -- Server version: 10.4.27-MariaDB
--- PHP Version: 8.2.0
+-- PHP Version: 8.0.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -31,7 +31,6 @@ CREATE TABLE `critics` (
   `critics_id` int(11) NOT NULL,
   `name` varchar(25) NOT NULL,
   `email` varchar(25) NOT NULL,
-  `critic_type` varchar(20) DEFAULT NULL,
   `contact` varchar(10) NOT NULL,
   `qualification` varchar(25) NOT NULL,
   `password` varchar(300) NOT NULL,
@@ -42,8 +41,8 @@ CREATE TABLE `critics` (
 -- Dumping data for table `critics`
 --
 
-INSERT INTO `critics` (`critics_id`, `name`, `email`, `critic_type`, `contact`, `qualification`, `password`, `about`) VALUES
-(4, 'parth', 'parth@gmail.com', 'art', '2323132341', 'B-tech', '$2y$10$lgMbGiNYJWYNosXlpUXI8OgrjJ0nrjT0JmAWe9Jll3cfcgtpA3Zey', 'i am a disco dancer');
+INSERT INTO `critics` (`critics_id`, `name`, `email`, `contact`, `qualification`, `password`, `about`) VALUES
+(4, 'parth', 'parth@gmail.com', '2323132341', 'B-tech', '$2y$10$lgMbGiNYJWYNosXlpUXI8OgrjJ0nrjT0JmAWe9Jll3cfcgtpA3Zey', 'i am a disco dancer');
 
 -- --------------------------------------------------------
 
@@ -69,11 +68,8 @@ CREATE TABLE `critics_content` (
 --
 
 INSERT INTO `critics_content` (`content_id`, `content`, `user_id`, `name`, `description`, `rating`, `critics_rated`, `file_type`, `art_type`, `upload_date`) VALUES
-(6, 0x363433663836613536653435332e6a7067, 8, '', 'drawing girl', 0, 0, 'image/jpeg', 'writing', '0000-00-00 00:00:00'),
-(15, 0x363434663936393938393562312e6a706567, 9, '', 'an ER diagram of dbms project.', 4, 1, 'image/jpeg', 'art', '0000-00-00 00:00:00'),
-(16, 0x363434663936636634386235632e6a706567, 10, '', 'Airline management system er diagram', 0, 0, 'image/jpeg', 'art', '0000-00-00 00:00:00'),
-(17, 0x363434666135623839663062352e6a7067, 9, '', 'Sample artwork for test', 0, 0, 'image/jpeg', 'art', '0000-00-00 00:00:00'),
-(18, 0x363434666135636365313737382e6a7067, 9, '', 'Sample artwork for test', 0, 0, 'image/jpeg', 'art', '0000-00-00 00:00:00');
+(3, 0x363433663833333862343733662e6a7067, 0, '', 'song singing', 0, 0, 'image/jpeg', 'on', '0000-00-00 00:00:00'),
+(6, 0x363433663836613536653435332e6a7067, 0, '', 'drawing girl', 0, 0, 'image/jpeg', 'on', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -94,25 +90,6 @@ CREATE TABLE `favourites` (
 
 CREATE TABLE `judges` (
   `critics_id` int(11) NOT NULL,
-  `content_id` int(11) NOT NULL,
-  `review` varchar(1000) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `judges`
---
-
-INSERT INTO `judges` (`critics_id`, `content_id`, `review`) VALUES
-(4, 15, 'Nice ER diagram');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `likes`
---
-
-CREATE TABLE `likes` (
-  `user_id` int(11) NOT NULL,
   `content_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -124,20 +101,10 @@ CREATE TABLE `likes` (
 
 CREATE TABLE `reviews` (
   `user_id` int(11) NOT NULL,
-  `name` varchar(25) NOT NULL,
   `content_id` int(11) NOT NULL,
+  `isliked` int(11) NOT NULL,
   `comment` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `reviews`
---
-
-INSERT INTO `reviews` (`user_id`, `name`, `content_id`, `comment`) VALUES
-(9, 'Ritesh', 3, 'Sneha is the best singer'),
-(9, 'Ritesh', 3, 'Best song ever'),
-(9, 'Ritesh', 3, 'hi'),
-(9, 'Ritesh', 3, 'According to my opinion, i should stop simping');
 
 -- --------------------------------------------------------
 
@@ -182,22 +149,13 @@ INSERT INTO `users` (`user_id`, `name`, `email`, `contact`, `age`, `password`) V
 
 CREATE TABLE `users_content` (
   `content_id` int(11) NOT NULL,
-  `creator_id` int(11) NOT NULL,
   `content` mediumblob NOT NULL,
-  `description` varchar(1000) NOT NULL,
   `ratings` int(11) NOT NULL,
   `likes` int(11) NOT NULL,
-  `art_type` varchar(50) NOT NULL,
-  `file_type` varchar(50) NOT NULL,
+  `type` int(11) NOT NULL,
+  `file` int(11) NOT NULL,
   `upload_date` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `users_content`
---
-
-INSERT INTO `users_content` (`content_id`, `creator_id`, `content`, `description`, `ratings`, `likes`, `art_type`, `file_type`, `upload_date`) VALUES
-(3, 0, 0x363433663833333862343733662e6a7067, 'song singing', 25, 657, 'on', 'image/jpeg', '0000-00-00 00:00:00');
 
 --
 -- Indexes for dumped tables
@@ -228,13 +186,6 @@ ALTER TABLE `favourites`
 ALTER TABLE `judges`
   ADD KEY `judges_critics_id` (`critics_id`),
   ADD KEY `judges_content_id` (`content_id`);
-
---
--- Indexes for table `likes`
---
-ALTER TABLE `likes`
-  ADD KEY `likes_user` (`user_id`),
-  ADD KEY `likes_content` (`content_id`);
 
 --
 -- Indexes for table `reviews`
@@ -276,7 +227,7 @@ ALTER TABLE `critics`
 -- AUTO_INCREMENT for table `critics_content`
 --
 ALTER TABLE `critics_content`
-  MODIFY `content_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `content_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `users`
@@ -288,7 +239,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users_content`
 --
 ALTER TABLE `users_content`
-  MODIFY `content_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `content_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
@@ -307,13 +258,6 @@ ALTER TABLE `favourites`
 ALTER TABLE `judges`
   ADD CONSTRAINT `judges_content_id` FOREIGN KEY (`content_id`) REFERENCES `critics_content` (`content_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `judges_critics_id` FOREIGN KEY (`critics_id`) REFERENCES `critics` (`critics_id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `likes`
---
-ALTER TABLE `likes`
-  ADD CONSTRAINT `likes_content` FOREIGN KEY (`content_id`) REFERENCES `users_content` (`content_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `likes_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `reviews`
